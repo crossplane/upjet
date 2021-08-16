330e197e109bbcadee1b6eecabc380ca9a712fd2
Hasan Turken <turkenh@gmail.com>
2021-08-17 00:43:28 +0300

    Add Terraformed resource interface
    Signed-off-by: Hasan Turken <turkenh@gmail.com>

---
+++ pkg/terraform/resource/interfaces.go
@@ -0,0 +1,27 @@
+package resource
+
+import (
+	"github.com/crossplane/crossplane-runtime/pkg/resource"
+)
+
+// TerraformStateHandler handles terraform state
+type TerraformStateHandler interface {
+	GetObservation() ([]byte, error)
+	SetObservation(data []byte) error
+
+	GetParameters() ([]byte, error)
+	SetParameters(data []byte) error
+}
+
+type TerraformMetadataProvider interface {
+	GetTerraformResourceType() string
+	GetTerraformResourceIdField() string
+}
+
+// Terraformed is a Kubernetes object representing a concrete terraform managed resource
+type Terraformed interface {
+	resource.Managed
+
+	TerraformMetadataProvider
+	TerraformStateHandler
+}
