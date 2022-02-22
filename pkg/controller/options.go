451974ca0089cd5667e60b2e6004df9fe3caa4eb
Muvaffak Onus <me@muvaf.com>
2022-02-22 12:08:54 +0300

    controller: use a single struct to pass options to individual controller init functions
    Signed-off-by: Muvaffak Onus <me@muvaf.com>

---
+++ pkg/controller/options.go
@@ -0,0 +1,42 @@
+/*
+Copyright 2022 The Crossplane Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	"github.com/crossplane/crossplane-runtime/pkg/controller"
+
+	"github.com/crossplane/terrajet/pkg/config"
+	"github.com/crossplane/terrajet/pkg/terraform"
+)
+
+// Options contains incriminating options for a given Terrajet controller instance.
+type Options struct {
+	controller.Options
+
+	// Provider contains all resource configurations of the provider which can
+	// be used to pick the related one. Since the selection is done in runtime,
+	// we need to pass everything and generated code will pick the one.
+	Provider *config.Provider
+
+	// WorkspaceStore will be used to pick/initialize the workspace the specific CR
+	// instance should use.
+	WorkspaceStore *terraform.WorkspaceStore
+
+	// SetupFn contains the provider-specific initialization logic, such as
+	// preparing the auth token for Terraform CLI.
+	SetupFn terraform.SetupFn
+}
