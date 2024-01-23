2b1d5dfa7945ffa710e5ff4dcc6366a82d33ee71
Alper Rifat Ulucinar <ulucinar@users.noreply.github.com>
2024-01-24 01:40:59 +0300

    Set the default value of the transformer command-line option --pattern to "./..."
    Signed-off-by: Alper Rifat Ulucinar <ulucinar@users.noreply.github.com>

--- cmd/resolver/main.go
+++ cmd/resolver/main.go
@@ -15,7 +15,7 @@ func main() {
 	var (
 		app                     = kingpin.New(filepath.Base(os.Args[0]), "Transformer for the generated resolvers by the crossplane-tools so that cross API-group imports are removed.").DefaultEnvars()
 		apiGroupSuffix          = app.Flag("apiGroupSuffix", "Resource API group suffix, such as aws.upbound.io. The resource API group names are suffixed with this to get the canonical API group name.").Short('g').Required().String()
-		pattern                 = app.Flag("pattern", "List patterns for the packages to process, such as ./apis/...").Short('p').Default("./apis/...").Strings()
+		pattern                 = app.Flag("pattern", "List patterns for the packages to process, such as ./...").Short('p').Default("./...").Strings()
 		resolverFilePattern     = app.Flag("resolver", "Name of the generated resolver files to process.").Short('r').Default("zz_generated.resolvers.go").String()
 		ignorePackageLoadErrors = app.Flag("ignoreLoadErrors", "Ignore errors encountered while loading the packages.").Short('s').Bool()
 	)
