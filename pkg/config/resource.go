ab14385febc25968ce21b486b3b2e608cb8c0abd
JP Schoombee <jpschoombee@gmail.com>
2026-06-08 16:22:20 +0200

    refactor: rename xpv1 alias to xpv2 for crossplane/apis/v2/core/v2
    The common APIs were relocated to a v2 package, so the xpv1 alias no
    longer reflects the import path. Standardise on xpv2 across all
    hand-written files; also unify the lone v1 alias in sensitive.go onto
    the same name. mockgen-generated fake still uses its default basename
    alias (v2) since it is regenerated.
    
    Addresses review feedback on #664.
    
    Signed-off-by: JP Schoombee <jpschoombee@gmail.com>

--- pkg/config/resource.go
+++ pkg/config/resource.go
@@ -13,7 +13,7 @@ import (
 	"github.com/crossplane/crossplane-runtime/v2/pkg/fieldpath"
 	"github.com/crossplane/crossplane-runtime/v2/pkg/reconciler/managed"
 	xpresource "github.com/crossplane/crossplane-runtime/v2/pkg/resource"
-	xpv1 "github.com/crossplane/crossplane/apis/v2/core/v2"
+	xpv2 "github.com/crossplane/crossplane/apis/v2/core/v2"
 	fwresource "github.com/hashicorp/terraform-plugin-framework/resource"
 	rschema "github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
@@ -342,7 +342,7 @@ func NewTagger(kube client.Client, fieldName string) *Tagger {
 
 // Initialize is a custom initializer for setting external tags
 func (t *Tagger) Initialize(ctx context.Context, mg xpresource.Managed) error {
-	if sets.New[xpv1.ManagementAction](mg.GetManagementPolicies()...).Equal(sets.New[xpv1.ManagementAction](xpv1.ManagementActionObserve)) {
+	if sets.New[xpv2.ManagementAction](mg.GetManagementPolicies()...).Equal(sets.New[xpv2.ManagementAction](xpv2.ManagementActionObserve)) {
 		// We don't want to add tags to the spec.forProvider if the resource is
 		// only being Observed.
 		return nil
